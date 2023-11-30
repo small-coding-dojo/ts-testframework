0ed9c77c26759cf44ab3387be514bc8990cf6b10
Niklas Rueber <nrueber@igus.net>
2023-11-30 13:02:20 +0100

    green test
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: Niklas Rueber <niklas@rueber.cloud>
    Co-authored-by: Falk Kühnel <falk@kuehnel.eu>

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bunny!");
+try
+{
+    assert(false, false);
+    console.log("Test succeeded.");
+}
+catch (err){
+    console.log("Test failed.");
+}
+
+function assert(expected: boolean, actual: boolean) {
+    if(expected !== actual){
+        throw new Error();
+    }
+}
